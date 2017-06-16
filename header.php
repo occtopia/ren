24c2787129f6e690a7d8ca6815a1df1a7c8237b0
Jason Occhipinti <positivespacegroup@gmail.com>
2017-06-16 17:14:56 -0400

    Added titles, keywords, and other meta.

--- header.php
+++ header.php
@@ -1,22 +1,22 @@
 <!doctype html>
 
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?>>
 
   <head>
 
     <!-- Standard Meta -->
     <meta charset="utf-8">
-    <title></title>
+    <title>The page title</title>
     <meta http-equiv="Content-Type" content="text/html">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
-    <meta name="author" content="">
+    <meta name="description" content="The page description.">
+    <meta name="keywords" content="keyword1, keyword2 , keyword3">
+    <meta name="author" content="Joe Somebody">
 
     <!-- Open Graph -->
-    <meta property="og:title" content="">
-    <meta property="og:description" content="">
+    <meta property="og:title" content="The page title">
+    <meta property="og:description" content="The page description.">
     <meta property="og:image" content="">
     <meta property="og:image:url" content="">
     <meta property="og:image:type" content="image/png">
@@ -24,10 +24,10 @@
     <meta property="og:image:height" content="628">
 
     <!-- Twitter Cards -->
-    <meta name="twitter:card" content="" />
+    <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@somebody" />
-    <meta name="twitter:title" content="" />
-    <meta name="twitter:description" content="" />
+    <meta name="twitter:title" content="The page title" />
+    <meta name="twitter:description" content="The page description." />
     <meta name="twitter:image" content="<?php echo get_template_directory_uri(); ?>/assets/img/cardImg.png" />
 
     <!-- Standard Favicons -->
@@ -40,7 +40,7 @@
     <!-- Win 8/10 -->
     <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/img/icons/mstile-144x144.png">
     <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/img/icons/mstile-150x150.png">
-    <meta name="msapplication-TileColor" content="#2c67c0">
+    <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="application-name" content="">
 
     <!-- iOS -->
@@ -59,7 +59,7 @@
     <!-- Android/Chrome -->
     <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/android-chrome-192x192.png" sizes="192x192">
     <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/img/icons/android-chrome-256x256.png" sizes="256x256">
-    <meta name="theme-color" content="#2c67c0">
+    <meta name="theme-color" content="#ffffff">
 
     <?php wp_head(); ?>
 
