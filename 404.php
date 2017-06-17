ad8a606907276b5e30cfbd755ab027a83fc52cd9
Jason Occhipinti <positivespacegroup@gmail.com>
2017-06-16 20:35:25 -0400

    Added 404.php

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+  <!-- main.wrapper -->
+  <main class="wrapper">
+
+    <!-- article -->
+    <article>
+
+      <h1>Page not found</h1>
+      <h2><a href="<?php echo home_url(); ?>">Return Home?</a></h2>
+
+    </article>
+    <!-- /article -->
+
+  </main>
+  <!-- /main.wrapper -->
+
+<?php get_footer(); ?>
