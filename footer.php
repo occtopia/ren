622c6f10df3bef43dadb4deab1c2ac64817d9539
Jason Occhipinti <positivespacegroup@gmail.com>
2017-06-16 20:40:17 -0400

    Additions to footer.php
    - Copyright information
    - Google analytics script
    - Updated structure

--- footer.php
+++ footer.php
@@ -1,9 +1,30 @@
-<footer>
+    <!-- footer -->
+    <footer class="footer">
 
-</footer>
+      <!-- copyright -->
+      <div class="copyright">
 
-</body>
+        <p>&copy; <?php echo date('Y'); ?> Copyright <?php bloginfo('name'); ?></p>
 
-<?php wp_footer(); ?>
+      </div>
+      <!-- /copyright -->
+
+    </footer>
+    <!-- /footer -->
+
+    <?php wp_footer(); ?>
+
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-XXXXX-Y', 'auto');
+      ga('send', 'pageview');
+    </script>
+    <!-- /Google Analytics -->
+
+  </body>
 
 </html>
