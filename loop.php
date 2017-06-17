4b691e4121305af3723441809263e10ce86203f6
Jason Occhipinti <positivespacegroup@gmail.com>
2017-06-16 20:35:11 -0400

    Article/section tag changes

--- loop.php
+++ loop.php
@@ -2,10 +2,10 @@
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-	<section>
-		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<?php the_content(); ?>
-	</section>
+	</article>
 
 	<?php endwhile; ?>
 
@@ -13,9 +13,9 @@
 
 	<?php else : ?>
 
-	<section>
-		<h1>No Posts Found</h1>
-	</section>
+	<article>
+		<h2>No Posts Found</h2>
+	</article>
 
 	<?php endif;
 
